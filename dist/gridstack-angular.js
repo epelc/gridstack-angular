80f779f85969f43502aa54d1988ed8b6a626a603
Ed Pelc <ed@freightchick.com>
2016-11-09 13:54:35 -0500

    feat(gridstack-item): add support for toggling locked, movable, and resizable

--- dist/gridstack-angular.js
+++ dist/gridstack-angular.js
@@ -97,85 +97,141 @@ app.directive('gridstack', ['$timeout', function($timeout) {
 })();
 
 (function() {
-'use strict';
+    'use strict';
 
-var app = angular.module('gridstack-angular');
+    var app = angular.module('gridstack-angular');
 
-app.directive('gridstackItem', ['$timeout', function($timeout) {
+    app.directive('gridstackItem', ['$timeout', function($timeout) {
 
-  return {
-    restrict: 'A',
-    require: '^gridstack',
-    scope: {
-      gridstackItem: '=',
-      onItemAdded: '&',
-      onItemRemoved: '&',
-      gsItemId: '=?',
-      gsItemX: '=',
-      gsItemY: '=',
-      gsItemWidth: '=',
-      gsItemHeight: '=',
-      gsItemAutopos: '='
-    },
-    link: function(scope, element, attrs, gridstackController) {
-      if (scope.gsItemId) {
-        element.attr('data-gs-id', scope.gsItemId);
-      }
-      element.attr('data-gs-x', scope.gsItemX);
-      element.attr('data-gs-y', scope.gsItemY);
-      element.attr('data-gs-width', scope.gsItemWidth);
-      element.attr('data-gs-height', scope.gsItemHeight);
-      element.attr('data-gs-auto-position', scope.gsItemAutopos);
-      var widget = gridstackController.addItem(element);
-      var item = element.data('_gridstack_node');
-      $timeout(function() {
-        scope.onItemAdded({item: item});
-      });
+        return {
+            restrict: 'A',
+            require: '^gridstack',
+            scope: {
+                gridstackItem: '=',
+                onItemAdded: '&',
+                onItemRemoved: '&',
+                gsItemId: '=?',
+                gsItemX: '=',
+                gsItemY: '=',
+                gsItemWidth: '=',
+                gsItemHeight: '=',
+                gsItemAutopos: '=',
+                gsItemResizable: '=',
+                gsItemMovable: '=',
+                // gsItemLocked sets whether moving/resizing other items move this item.
+                gsItemLocked: '='
+            },
+            link: function(scope, element, attrs, gridstackController) {
+                if (scope.gsItemId) {
+                    element.attr('data-gs-id', scope.gsItemId);
+                }
+                element.attr('data-gs-x', scope.gsItemX);
+                element.attr('data-gs-y', scope.gsItemY);
+                element.attr('data-gs-width', scope.gsItemWidth);
+                element.attr('data-gs-height', scope.gsItemHeight);
+                element.attr('data-gs-auto-position', scope.gsItemAutopos);
+                if (!scope.gsItemResizable) {
+                    element.attr('data-gs-no-resize', true);
+                }
+                if (!scope.gsItemMovable) {
+                    element.attr('data-gs-no-move', true);
+                }
+                if (scope.gsItemLocked) {
+                    element.attr('data-gs-locked', scope.gsItemLocked);
+                }
 
-      // Update gridstack element after scope changes
-      // NOTE we must only make a gridstack update call for these watchers if something changed.
-      // Otherwise it will cause issues with the 'change' event not firing because you ran an
-      // update op partway through it.
-      scope.$watchGroup(['gsItemX', 'gsItemY', 'gsItemWidth', 'gsItemHeight'], function() {
-        if (Number(element.attr('data-gs-x')) !== scope.gsItemX ||
-          Number(element.attr('data-gs-y')) !== scope.gsItemY ||
-          Number(element.attr('data-gs-width')) !== scope.gsItemWidth ||
-          Number(element.attr('data-gs-height')) !== scope.gsItemHeight) {
-          gridstackController.gridstackHandler.update(element, scope.gsItemX, scope.gsItemY,
-            scope.gsItemWidth, scope.gsItemHeight);
-        }
-      });
+                var widget = gridstackController.addItem(element);
+                var item = element.data('_gridstack_node');
+                $timeout(function() {
+                    scope.onItemAdded({
+                        item: item
+                    });
+                });
 
-      // Update scope after gridstack attributes change
-      scope.$watch(function() { return element.attr('data-gs-id'); }, function(val) {
-        scope.gsItemId = val;
-      });
+                // Update gridstack element after scope changes
+                // NOTE we must only make a gridstack update call for these watchers if something changed.
+                // Otherwise it will cause issues with the 'change' event not firing because you ran an
+                // update op partway through it.
+                scope.$watchGroup(['gsItemX', 'gsItemY', 'gsItemWidth', 'gsItemHeight'], function() {
+                    if (Number(element.attr('data-gs-x')) !== scope.gsItemX ||
+                        Number(element.attr('data-gs-y')) !== scope.gsItemY ||
+                        Number(element.attr('data-gs-width')) !== scope.gsItemWidth ||
+                        Number(element.attr('data-gs-height')) !== scope.gsItemHeight) {
+                        gridstackController.gridstackHandler.update(element, scope.gsItemX, scope.gsItemY,
+                            scope.gsItemWidth, scope.gsItemHeight);
+                    }
+                });
 
-      scope.$watch(function() { return element.attr('data-gs-x'); }, function(val) {
-        scope.gsItemX = Number(val);
-      });
+                scope.$watch('gsItemResizable', function() {
+                    gridstackController.gridstackHandler.resizable(element, scope.gsItemResizable);
+                })
+                scope.$watch('gsItemMovable', function() {
+                    gridstackController.gridstackHandler.movable(element, scope.gsItemMovable);
+                })
+                scope.$watch('gsItemLocked', function() {
+                    gridstackController.gridstackHandler.locked(element, scope.gsItemLocked);
+                })
 
-      scope.$watch(function() { return element.attr('data-gs-y'); }, function(val) {
-        scope.gsItemY = Number(val);
-      });
+                // Update scope after gridstack attributes change
+                scope.$watch(function() {
+                    return element.attr('data-gs-id');
+                }, function(val) {
+                    scope.gsItemId = val;
+                });
 
-      scope.$watch(function() { return element.attr('data-gs-width'); }, function(val) {
-        scope.gsItemWidth = Number(val);
-      });
+                scope.$watch(function() {
+                    return element.attr('data-gs-x');
+                }, function(val) {
+                    scope.gsItemX = Number(val);
+                });
 
-      scope.$watch(function() { return element.attr('data-gs-height'); }, function(val) {
-        scope.gsItemHeight = Number(val);
-      });
+                scope.$watch(function() {
+                    return element.attr('data-gs-y');
+                }, function(val) {
+                    scope.gsItemY = Number(val);
+                });
 
-      element.bind('$destroy', function() {
-        var item = element.data('_gridstack_node');
-        scope.onItemRemoved({item: item});
-        gridstackController.removeItem(element);
-      });
+                scope.$watch(function() {
+                    return element.attr('data-gs-width');
+                }, function(val) {
+                    scope.gsItemWidth = Number(val);
+                });
 
-    }
+                scope.$watch(function() {
+                    return element.attr('data-gs-height');
+                }, function(val) {
+                    scope.gsItemHeight = Number(val);
+                });
 
-  };
+                // scope.$watch(function() {
+                //     return element.attr('data-gs-no-resize');
+                // }, function(val) {
+                //     scope.gsItemResizable = !!val;
+                // });
+                //
+                // scope.$watch(function() {
+                //     return element.attr('data-no-move');
+                // }, function(val) {
+                //     scope.gsItemMovable = !!val;
+                // });
+                //
+                // scope.$watch(function() {
+                //     return element.attr('data-gs-locked');
+                // }, function(val) {
+                //     scope.gsItemLocked = !!val;
+                // });
 
-}]);
+                element.bind('$destroy', function() {
+                    var item = element.data('_gridstack_node');
+                    scope.onItemRemoved({
+                        item: item
+                    });
+                    gridstackController.removeItem(element);
+                });
+
+            }
+
+        };
+
+    }]);
 })();
